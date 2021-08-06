667b7e9a1e1208798f06c43696b4ed28e871048f
jnprak <41762915+jnprak@users.noreply.github.com>
2021-08-06 14:08:50 -0400

    add style

--- src/App.js
+++ src/App.js
@@ -1,28 +1,29 @@
 import React, {useState, useEffect} from 'react';
+import './App.css'
 import axios from 'axios'
 import Coin from './Coin';
-
-
-
 
 function App() {
   const [coins, setCoins] = useState([])
   const [search, setSearch] = useState('');
   useEffect(() => {
-    axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false
-      `).then(res => {
+    axios.get(`https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=100&page=1&sparkline=false`).then(res => {
         setCoins(res.data)
       }).catch(error => console.log(error))
   }, []);
 
 
+  // Set Search
   const handleChange = e => {
     setSearch(e.target.value)
   }
 
+  // Filters searched coins 
   const filteredCoins = coins.filter(coin => 
-    coin.name.toLowerCase().includes(search.toLowerCase)
+    coin.name.toLowerCase().includes(search.toLowerCase())
     )
+
+
   return (
     <div className="coin-app">
       <div className="coin-search">
@@ -33,7 +34,17 @@ function App() {
       </div>
       {filteredCoins.map(coin => {
         return (
-          <Coin />
+          <Coin 
+          key={coin.id} 
+          name={coin.name} 
+          image={coin.image}
+          symbol={coin.symbol}
+          volume={coin.total_volume}
+          price={coin.current_price}
+          priceChange={coin.price_change_percentage_24h}
+          marketcap={coin.market_cap}
+          />
+        
         )
       })}
     </div>
